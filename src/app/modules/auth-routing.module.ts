510e91b4be64d1effe46885d80a85b0d1ef11a42
dhaluksha <79889950+dhaluksha@users.noreply.github.com>
2023-01-30 21:48:10 +0530

    Revert "bug fix"
    This reverts commit 2fd27b9285d154da603f34960f733217e955022b.

---
+++ src/app/modules/auth-routing.module.ts
@@ -0,0 +1,17 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterModule, Routes } from '@angular/router';
+// import { AuthTabsComponent } from '../components/auth-tabs/auth-tabs.component';
+
+// const routes: Routes =[
+//   { path: '', component:  AuthTabsComponent}
+// ]
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     RouterModule.forRoot(routes)
+//   ],
+//   exports: [ RouterModule ]
+// })
+// export class AuthRoutingModule { }
